243263b00b6237699ada10ded536a72e1e5428ed
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-09-08 05:29:30 +0000

    マイページ機能の実装

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,10 +1,27 @@
 class Public::MembersController < ApplicationController
   def show
+    @member = Member.find(params[:id])
+    @posts = Post.where(member_id = params[:id])
   end
 
   def edit
+    @member = Member.find(params[:id])
+  end
+  def update
+    member = Member.find(params[:id])
+    if member.update(member_params)
+      redirect_to member_path(member.id)
+    else
+      @member = Member.find(params[:id])
+      render :edit
+    end
   end
 
   def confirm_quit
+  end
+
+  private
+  def member_params
+    params.require(:member).permit(:nick_name, :profile, :email, :image)
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,5 +3,6 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  has_one_attached :image
 end

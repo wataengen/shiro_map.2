e4b0540f6d4f866d192744d4fe736d819056b72a
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-09-16 09:17:23 +0000

    .envファイルの編集

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,11 +1,14 @@
 class Public::HomesController < ApplicationController
 
   def top
+    puts "作成したキー#{ENV['GOOGLE_MAPS_API_KEY']}"
     @posts = Post.where(display_status: true).sort_by{|post| -(post[:id])}
+    #ログイン前は公開済みの投稿が最新順に表示される
     if member_signed_in?
       @member = Member.find(current_member.id)
       follows = @member.following_members
       @followers_posts = Post.where(member_id: follows).where(display_status: true).sort_by{|post| -(post[:id])}
+      #ログイン後はフォロー会員の投稿が最新順に表示される
     end
     @post_comment = PostComment.new
   end

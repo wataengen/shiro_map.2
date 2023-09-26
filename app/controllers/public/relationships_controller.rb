d6e5c59e9e2e3d629a82099ab4543574c9fd94e5
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-09-26 03:22:50 +0000

    フォロー機能の非同期通信化

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,12 +1,14 @@
 class Public::RelationshipsController < ApplicationController
   before_action :authenticate_member!
   def create
-    current_member.follow(params[:member_id])
-    redirect_to request.referer
+    member = current_member.follow(params[:member_id])
+    @member = Member.find(params[:member_id])
+    # redirect_to request.referer
   end
 
   def destroy
-    current_member.unfollow(params[:member_id])
-    redirect_to request.referer
+    member = current_member.unfollow(params[:member_id])
+    @member = Member.find(params[:member_id])
+    # redirect_to request.referer
   end
 end
